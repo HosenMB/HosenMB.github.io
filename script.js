0ead8354250653f3ed0277f2899f658bf57de0a8
Md Biplob Hosen <biplob.umbc@gmail.com>
2025-08-10 00:16:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-const sidebarToggle = document.querySelector('.sidebar-toggle');
-const sidebar = document.querySelector('aside.sidebar');
-const mainContent = document.querySelector('.main-content');
-
-sidebarToggle.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-  mainContent.classList.toggle('sidebar-active');
-});
-
-// Close sidebar when clicking outside on mobile
-mainContent.addEventListener('click', () => {
-  if (window.innerWidth <= 768 && sidebar.classList.contains('active')) {
-    sidebar.classList.remove('active');
-    mainContent.classList.remove('sidebar-active');
+document.addEventListener("DOMContentLoaded", function () {
+  const cvButton = document.getElementById("cvButton");
+  if (cvButton) {
+    cvButton.addEventListener("click", function () {
+      window.open("Resume of Md Biplob Hosen.pdf", "_blank");
+    });
   }
 });
+
